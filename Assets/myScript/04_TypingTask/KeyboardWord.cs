6143f784858ed3852e20ab11f35b265bc6519b5d
Tinghui Li <147955760+Tinghui-Li@users.noreply.github.com>
2024-07-09 21:18:05 +1000

    Update KeyboardWord.cs

--- Assets/myScript/04_TypingTask/KeyboardWord.cs
+++ Assets/myScript/04_TypingTask/KeyboardWord.cs
@@ -5,11 +5,22 @@ using UnityEngine.UI;
 public class KeyboardWord : MonoBehaviour
 {
     public InputField inputField;
+    private bool deleteClicked = false;
 
     private void Update()
     {
         Logger.enteredSentence = inputField.text;
         LogTutorial.enteredSentence = inputField.text;
+        
+        if (deleteClicked)
+        {
+            Logger.fixedError = 1;
+            deleteClicked = false;  // Reset the flag after updating the logger
+        }
+        else
+        {
+            Logger.fixedError = 0;
+        }
     }
 
     public void OnKeyClicked(string key)
@@ -22,6 +33,7 @@ public class KeyboardWord : MonoBehaviour
         if (inputField.text.Length > 0)
         {
             inputField.text = inputField.text.Substring(0, inputField.text.Length - 1);
+            deleteClicked = true;
         }
     }
 }
